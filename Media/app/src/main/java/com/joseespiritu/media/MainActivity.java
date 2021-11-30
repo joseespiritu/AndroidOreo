2ad9ec9c2fdb5ea854eade471e24c8eba9b3d27a
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-11-30 16:41:41 -0600

    other animations

--- Media/app/src/main/java/com/joseespiritu/media/MainActivity.java
+++ Media/app/src/main/java/com/joseespiritu/media/MainActivity.java
@@ -8,24 +8,15 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean image1IsShowng = true;
-
     public void fade(View view) {
         Log.i("Info", "ImageView tapped");
 
         ImageView imageView1 = (ImageView) findViewById(R.id.imageView1);
         ImageView imageView2 = (ImageView) findViewById(R.id.imageView2);
 
-        if(image1IsShowng) {
-            image1IsShowng = false;
-            imageView1.animate().alpha(0).setDuration(2000);
-            imageView2.animate().alpha(1).setDuration(2000);
-        } else {
-            image1IsShowng = true;
-            imageView1.animate().alpha(1).setDuration(2000);
-            imageView2.animate().alpha(0).setDuration(2000);
-        }
-
+        //imageView1.animate().translationXBy(-1000).setDuration(2000);
+        //imageView1.animate().rotation(1800).alpha(0).setDuration(1000);
+        imageView1.animate().scaleX(0.5f).scaleY(0.5f).setDuration(1000);
 
     }
 
@@ -33,5 +24,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView1 = (ImageView) findViewById(R.id.imageView1);
+        imageView1.setX(-1000);
+        imageView1.animate().translationXBy(1000).rotation(3600).setDuration(2000);
+
     }
 }
